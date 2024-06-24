20ac3d2b95974fd5eccbf5b4ace27a9d174bc6e4
Shadow Scripter <137788903+Shadow-Scripter@users.noreply.github.com>
2024-06-24 17:08:38 +0500

    Update Stopwatch.jsx

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -54,7 +54,7 @@ function Stopwatch(){
             <div className="display">{formatTime()}</div>
             <div className="controls">
                 <button onClick={start} className="start-button">Start</button>
-                <button onClick={reset} className="reset-button">Restet</button>
+                <button onClick={reset} className="reset-button">Reset</button>
                 <button onClick={stop} className="stop-button">Stop</button>
             </div>
         </div>
